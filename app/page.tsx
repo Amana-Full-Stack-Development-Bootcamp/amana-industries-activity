5c2034f2cc6836611336ba7c5c848c13af50a36f
DiaaHSharqawi <diaasharqawi00@gmail.com>
2025-09-20 19:26:54 +0300

    fix production bar chart problem

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,23 @@ import dynamic from "next/dynamic";
 import React from "react";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
+import ProductionBarChart from "./components/ProductionBarChart";
 import { jsonData } from "./data/jsonData";
 
 // Dynamically import MapComponent (client-side only)
-
 export default function Home() {
   const MapComponent = dynamic(() => import("./components/Map"), {
     ssr: false,
   });
 
   return (
-    <div className="min-h-screen bg-white flex flex-col font-sans">
+    // Add vertical spacing between direct children using space-y
+    <div className="min-h-screen bg-white flex flex-col font-sans space-y-6">
       <Header companyInfo={jsonData.company_info} />
       <main className="flex-grow">
         <MapComponent factories={jsonData.factory_data} />
       </main>
+      <ProductionBarChart data={jsonData} />
       <Footer />
     </div>
   );
